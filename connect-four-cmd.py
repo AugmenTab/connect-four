a2b1d2276fbabf58cbb5bb175623f2a9728e8bc9
AugmenTab <tbaum92@live.com>
2020-06-06 12:16:54 -0600

    Added code to check horizontal and vertical wins.

--- connect-four-cmd.py
+++ connect-four-cmd.py
@@ -6,14 +6,14 @@ COL_COUNT = 7
 import numpy as np
 
 def create_board():
-    board = np.zeros((6,7))
+    board = np.zeros((ROW_COUNT,COL_COUNT))
     return board
 
 def dropPiece(board, row, col, piece):
     board[row][col] = piece
 
 def isValidLocation(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT-1][col] == 0
 
 def getNextOpenRow(board, col):
     for r in range(ROW_COUNT):
@@ -23,9 +23,19 @@ def getNextOpenRow(board, col):
 def printBoard(board):
     print(np.flip(board, 0))
 
+def winningMove(board, piece):
+    for c in range(COL_COUNT-3):
+        for r in range(ROW_COUNT):
+            if piece == board[r][c] == board[r][c+1] == board[r][c+2] == board[r][c+3]:
+                return True
+    for c in range(COL_COUNT):
+        for r in range(ROW_COUNT-3):
+            if piece == board[r][c] == board[r+1][c] == board[r+2][c] == board[r+3][c]:
+                return True
+
 board = create_board()
-printBoard(board)
 game_over = False
+printBoard(board)
 turn = 0
 
 while not game_over:
@@ -34,11 +44,17 @@ while not game_over:
         if isValidLocation(board, col):
             row = getNextOpenRow(board, col)
             dropPiece(board, row, col, 1)
+            if winningMove(board, 1):
+                print('PLAYER 1 WINS!')
+                game_over = True
     else:
         col = int(input('Player 2, Make Your Selection (0-6):'))
         if isValidLocation(board, col):
             row = getNextOpenRow(board, col)
             dropPiece(board, row, col, 2)
+            if winningMove(board, 2):
+                print('PLAYER 2 WINS!')
+                game_over = True
     printBoard(board)
     turn += 1
     turn = turn % 2
